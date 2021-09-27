c5da46ebca7d2a7a7d3f96e3637da1c8c83abc34
JoaoPedroArnaut <joao.silva@bandtec.com.br>
2021-09-27 20:22:24 -0300

    renomeando endpoint

--- src/main/java/br/com/ezfix/api/controller/SolicitanteController.java
+++ src/main/java/br/com/ezfix/api/controller/SolicitanteController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/cliente")
+@RequestMapping("/solicitante")
 public class SolicitanteController extends baseController {
 
     @Autowired
